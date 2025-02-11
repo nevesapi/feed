59e36e319b74d27c5be4783d08064098887297c6
Denis Neves <nevesdenis35@gmail.com>
2025-02-10 21:37:01 -0300

    Configuração do plugin tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { toast } from "react-toastify";
-
 export function App() {
   return (
-    <div className=" p-4 bg-emerald-700">
+    <div className="bg-emerald-700 p-4">
       <h1 className="text-3xl font-bold text-white">Teste</h1>
     </div>
   );
